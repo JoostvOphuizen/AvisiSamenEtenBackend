17f64b109516e4d41ff0f6ff33aa2c777d53e179
Ayaz Rachmadiev <a.rachmadiev@student.han.nl>
2023-04-24 13:22:16 +0200

    Revert "Voor jenkins iets weggehaald"
    This reverts commit a041251d5e89011ce8c87b2edb3fa38577fec6be.

--- src/test/kotlin/nl/han/oose/scala/scalasameneten/ScalasamenetenApplicationTests.kt
+++ src/test/kotlin/nl/han/oose/scala/scalasameneten/ScalasamenetenApplicationTests.kt
@@ -4,7 +4,7 @@ import com.example.springboot.Application
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-
+@SpringBootTest
 class   ScalasamenetenApplicationTests {
 
     @Test
