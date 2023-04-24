0d7049c7ea3a16387629b797cfdd5ea49fb01a33
Ayaz Rachmadiev <a.rachmadiev@student.han.nl>
2023-04-24 13:32:38 +0200

    Springboottests weggehaald

--- src/test/kotlin/nl/han/oose/scala/scalasameneten/ScalasamenetenApplicationTests.kt
+++ src/test/kotlin/nl/han/oose/scala/scalasameneten/ScalasamenetenApplicationTests.kt
@@ -1,10 +1,10 @@
 package nl.han.oose.scala.scalasameneten
 
-import com.example.springboot.Application
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+import org.junit.jupiter.api.Test
+
+
+
 class   ScalasamenetenApplicationTests {
 
     @Test
