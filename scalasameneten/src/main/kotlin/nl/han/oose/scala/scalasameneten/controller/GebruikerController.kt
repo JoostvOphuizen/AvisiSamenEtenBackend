6621ab774150ec23254e07122d0ea49049e793b9
B-HANgit <BAH.vanderVen@student.han.nl>
2023-04-25 12:44:40 +0200

    post gebruikersvoorkeuren werkend

--- scalasameneten/src/main/kotlin/nl/han/oose/scala/scalasameneten/controller/GebruikerController.kt
+++ scalasameneten/src/main/kotlin/nl/han/oose/scala/scalasameneten/controller/GebruikerController.kt
@@ -1,6 +1,5 @@
 package nl.han.oose.scala.scalasameneten.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import nl.han.oose.scala.scalasameneten.dto.gebruiker.GebruikerDTO
 import nl.han.oose.scala.scalasameneten.dto.gebruiker.GebruikersDTO
 import nl.han.oose.scala.scalasameneten.dto.voorkeur.VoorkeurenDTO
@@ -26,15 +25,8 @@ class GebruikerController(private val gebruikerService: GebruikerService, privat
 
 
     @PostMapping("/slavoorkeurenop" ,produces = ["application/json"], consumes = ["application/json"])
-    fun slaVoorkeurenOp(@RequestParam id: Int, @RequestBody voorkeuren: VoorkeurenDTO) : ResponseEntity<VoorkeurenDTO> {
-        val mapper = ObjectMapper()
-        val json = mapper.writeValueAsString(voorkeuren)
+    fun postVoorkeuren(@RequestParam id: Int, @RequestBody voorkeuren: VoorkeurenDTO) : ResponseEntity<Void> {
         val voorkeurenDTO = modelMapper.map(voorkeuren, VoorkeurenDTO::class.java)
-        println("voorkeuren: $json")
-        println("voorkeuren: ${voorkeurenDTO.voorkeuren}")
-        for (voorkeur in voorkeurenDTO.voorkeuren!!) {
-            println("voorkeur: ${voorkeur.naam}")
-        }
-        return ResponseEntity.ok(voorkeurenDTO)
+        return gebruikerService.postGebruikersVoorkeuren(id, voorkeurenDTO)
     }
 }
